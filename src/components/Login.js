61f0b4266cb5a332ce3885c6fa4bbfa50d4e3156
abhinav20git <118235094+abhinav20git@users.noreply.github.com>
2024-02-19 23:21:33 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>/
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"/>
+        {/* https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2023.ico */}
+    </div>
+  )
+}
+
+export default Login
